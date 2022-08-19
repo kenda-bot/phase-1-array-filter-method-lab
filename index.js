b84c6ff8a78ffb0cef573223d692584fb02199c6
margaret <margaret.kenda@yahoo.com>
2022-08-19 13:46:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(drivers, attribute) {
+    return drivers.filter(function (driver) { return driver.toLowerCase() === attribute.toLowerCase() })
+  }
+  
+  function fuzzyMatch(source, testString) {
+    return source.filter(
+      (possibleMatch) =>
+        possibleMatch.indexOf(testString) === 0
+    );
+  }
+  
+  function matchName(source, soughtName) {
+    return source.filter((record) => record.name === soughtName);
+  }
